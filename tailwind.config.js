9c2659337bd54f4d4abe3fd43aa65180422cf37b
Tereza Smolová <framboiseart@email.cz>
2024-01-03 18:24:37 +0100

    Working in progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '420px',
+      },
+    },
   },
   plugins: [],
 }
